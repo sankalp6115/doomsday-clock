3d512047ab0650e5115a5e39ded2b0bebe8cba85
sankalp6115 <sankalpomar6115@gmail.com>
2026-01-20 00:38:42 +0530

    initial commit

---
+++ backend/server.py
@@ -0,0 +1,27 @@
+from flask import Flask, send_from_directory
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__, static_folder='..', static_url_path='')
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+viewers = 0
+
+@app.route('/')
+def index():
+    return send_from_directory('..', 'index.html')
+
+@socketio.on('connect')
+def handle_connect():
+    global viewers
+    viewers += 1
+    emit('viewer_count', viewers, broadcast=True)
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    global viewers
+    viewers -= 1
+    emit('viewer_count', viewers, broadcast=True)
+
+if __name__ == '__main__':
+    socketio.run(app)
